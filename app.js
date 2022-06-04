3d0455cb1473ac607910d78764c753a15077967f
Mariam <mariam.dateshidze.1@btu.edu.ge>
2022-06-04 14:41:36 +0400

    add responsive

--- app.js
+++ app.js
@@ -142,7 +142,6 @@ function handleClick(nextIndex){
 }
 
 
-
 //  filter projects
 for (i = 0; i < projectsTitles.length; i++) {
   projectsTitles[i].addEventListener("click", (e) => {
@@ -164,64 +163,3 @@ for (i = 0; i < projectsTitles.length; i++) {
     })
   })
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// filterObjects("all");
-
-// function filterObjects(c){
-//   let x, i;
-//   x = document.querySelectorAll('.image')
-//   if (c === "all") c ="";
-//   for (i = 0; i < x.length; i++){
-//     removeClass(x[i], "show");
-//     if (x[i].className.indexOf(c) > -1) addClass(x[i], "show")
-//   }
-// }
-
-// function addClass(element, name){
-//   let i, arr1, arr2;
-//   arr1 = element.className.split(" ");
-//   arr2 = name.split(" ");
-//   for (i = 0; i < arr2.length; i++){
-//     if (arr1.indexOf(arr2[i] == -1)){
-//       element.className += " " + arr2[i];
-//     }
-//   }
-
-// }
-
-// function removeClass(element, name){
-//   let i, arr1, arr2;
-//   arr1 = element.className.split(" ");
-//   arr2 = name.split(" ");
-//   for (i = 0; i < arr2.length; i++){
-//     while (arr1.indexOf(arr2[i])){
-//       arr1.splice(arr1.indexOf(arr2[i]), 1);
-//     }
-//   }
-//   element.className = arr1.join(" ");
-
-// }
